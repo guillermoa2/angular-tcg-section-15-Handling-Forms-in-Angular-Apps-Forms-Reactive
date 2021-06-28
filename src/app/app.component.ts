f6eedb167dc6886994968edce719a66ae2fc3ac4
Guillermo Acosta <guillermo222@att.net>
2021-06-28 13:51:25 -0700

    Reactive: Syncing HTML and Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'usernme': new FormControl(null), //wrapping the key as a string, just incase. To not interfere with the HTML code,
+      'username': new FormControl(null), //wrapping the key as a string, just incase. To not interfere with the HTML code,
       'email': new FormControl(null),
       'gender': new FormControl('female'), // CAN pass null for radio, BUT a default value is set instead
     });
