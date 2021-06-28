cb9f4ab67154add9d332584074078b8b64f857cc
Guillermo Acosta <guillermo222@att.net>
2021-06-28 16:08:14 -0700

    Reactive: Creating Custom Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,17 @@ import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({ // Nested Form Group
-        'username': new FormControl(null, Validators.required), //wrapping the key as a string, just incase. To not interfere with the HTML code,
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+         //wrapping the key as a string, just incase. To not interfere with the HTML code,
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'gender': new FormControl('female'), // CAN pass null for radio, BUT a default value is set instead,
+      'gender': new FormControl('female'),
+       // CAN pass null for gender radio, BUT a default value is set instead,
       'hobbies': new FormArray([])
     });
   }
@@ -44,6 +47,12 @@ export class AppComponent implements OnInit {
   //    return (this.signupForm.get('hobbies') as FormArray).controls;
   // }
 
-
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    // -1 is interpretted as true, meaning DOES NOT exist in array. But we are checking if it DOES exist in the array, meaning it is invalid. So DO run the if statement
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    } 
+    return null;  // return {'nameIsForbidden': false} will NOT work! OR just omit the return statement
+  }
 
 }
